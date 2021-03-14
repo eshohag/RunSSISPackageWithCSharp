2a71862745f46dc536f87deadaffb2ebce0c0065
Md Shohag Mia <shohag.mia@datasoft-bd.com>
2021-03-14 15:43:29 +0600

    Add project files.

---
+++ AfterDeployingIntoDB/Program.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.SqlServer.Management.IntegrationServices;
+using System.Data.SqlClient;
+
+namespace AfterDeployingIntoDB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Variables
+            string targetServerName = @"192.168.2.111\SQL17ENTERPRISE,1435";
+            string folderName = "SSISPackageItems";
+            string projectName = "SSISPackageRunInSQLJob";
+            string packageName = "Package.dtsx";
+
+            // Create a connection to the server
+            string sqlConnectionString = "Data Source=" + targetServerName +";Initial Catalog=master;Integrated Security=SSPI;";
+            SqlConnection sqlConnection = new SqlConnection(sqlConnectionString);
+
+            // Create the Integration Services object
+            IntegrationServices integrationServices = new IntegrationServices(sqlConnection);
+
+            // Get the Integration Services catalog
+            Catalog catalog = integrationServices.Catalogs["SSISDB"];
+
+            // Get the folder
+            CatalogFolder folder = catalog.Folders[folderName];
+
+            // Get the project
+            ProjectInfo project = folder.Projects[projectName];
+
+            // Get the package
+            PackageInfo package = project.Packages[packageName];
+
+            // Run the package
+            package.Execute(false, null);
+        }
+    }
+}
---
+++ AfterDeployingIntoDB/ReadMeFirst/ReadMe.txt
@@ -0,0 +1,14 @@
+﻿Add references
+In Solution Explorer, right-click the References folder and select Add Reference. The Reference Manager dialog box opens.
+In the Reference Manager dialog box, expand Assemblies and select Extensions.
+Select the following two references to add:
+Microsoft.SqlServer.Management.Sdk.Sfc
+Microsoft.SqlServer.Smo
+
+Click the Browse button to add a reference to Microsoft.SqlServer.Management.IntegrationServices. 
+(This assembly is installed only in the global assembly cache (GAC).) The Select the files to reference dialog box opens.
+In the Select the files to reference dialog box, navigate to the GAC folder that contains the assembly.
+Typically this folder is C:\Windows\assembly\GAC_MSIL\Microsoft.SqlServer.Management.IntegrationServices\14.0.0.0__89845dcd8080cc91.
+Select the assembly (that is, the .dll file) in the folder and click Add.
+Click OK to close the Reference Manager dialog box and add the three references. 
+To make sure the references are there, check the References list in Solution Explorer.
